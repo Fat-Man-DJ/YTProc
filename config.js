ee92e9e306a25200149fa8ce75ee24d805f986b4
cakexxx <49086951+cakexxx@users.noreply.github.com>
2020-06-13 09:43:52 +0800

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const {CookieAccessInfo, CookieJar, Cookie} = cookiejar
 
 let config = {
     httpprefix: 'https', port: 443,
-    serverName: 'sproxy-4.now.sh',
+    serverName: 'siteproxy-c6cws9rzr.vercel.app',
 }
 let blockedSites = ['www.chase.com'] // accessing to chase.com was reported by google
 
