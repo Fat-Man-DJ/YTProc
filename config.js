5ca6558fbd5afaaf858b9237047d441667739712
FatManDJ <52614115+Fat-Man-DJ@users.noreply.github.com>
2020-08-29 11:03:07 +0800

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const {CookieAccessInfo, CookieJar, Cookie} = cookiejar
 
 let config = {
     httpprefix: 'https', port: 443,
-    serverName: 'siteproxy-c6cws9rzr.vercel.app',
+    serverName: 'youtubeproxy-iota.vercel.app/',
 }
 let blockedSites = ['www.chase.com'] // accessing to chase.com was reported by google
 
